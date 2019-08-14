b03de5354d6821ee0a78a7af27f14ff7407424d0
Ichinose Shogo <shogo82148@gmail.com>
2019-08-15 07:32:25 +0900

    fix installing message

--- ubuntu/2.0/update.pl
+++ ubuntu/2.0/update.pl
@@ -38,8 +38,8 @@ sub execute_template {
     my $doc = do { local $/ = undef; <$fh>; };
     close $fh;
 
-    $doc =~ s/%%GOLANG_VERSION%%/$latest/;
-    $doc =~ s/%%GOLANG_DOWNLOAD_SHA256%%/$sha256/;
+    $doc =~ s/%%GOLANG_VERSION%%/$latest/g;
+    $doc =~ s/%%GOLANG_DOWNLOAD_SHA256%%/$sha256/g;
 
     open $fh, '>', "$output" or die $!;
     print $fh $doc;
