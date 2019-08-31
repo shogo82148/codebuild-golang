f6fac0b09c334a0497702f90d0adf7ba44f992c9
Ichinose Shogo <shogo82148@gmail.com>
2019-08-31 09:12:45 +0900

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-( cd ubuntu/1.0 && ./update.pl 1.11 && ./update.pl 1.12 )
-( cd ubuntu/2.0 && ./update.pl 1.11 && ./update.pl 1.12 )
-( cd al2/1.0 && ./update.pl 1.11 && ./update.pl 1.12 )
+(
+    cd ubuntu/1.0 \
+    && echo checking update for 1.11-standard-1.0 >&2 \
+    && ./update.pl 1.11 \
+    && echo checking update for 1.12-standard-1.0 >&2 \
+    && ./update.pl 1.12
+)
+(
+    cd ubuntu/2.0 \
+    && echo checking update for 1.11-standard-2.0 >&2 \
+    && ./update.pl 1.11 \
+    && echo checking update for 1.12-standard-2.0 >&2 \
+    && ./update.pl 1.12
+)
+(
+    cd al2/1.0 \
+    && echo checking update for 1.11-amazonlinux2-1.0 >&2 \
+    && ./update.pl 1.11 \
+    && echo checking update for 1.12-amazonlinux2-1.0 >&2 \
+    && ./update.pl 1.12
+)
