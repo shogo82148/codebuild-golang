2fc7a9d14b29783222d51bb72ce536fb3dbe7b85
Ichinose Shogo <shogo82148@gmail.com>
2019-09-26 07:50:44 +0900

    fix log message

--- update.sh
+++ update.sh
@@ -6,16 +6,16 @@
     && ./update.pl 1.11 \
     && echo checking update for 1.12-standard-1.0 >&2 \
     && ./update.pl 1.12 \
-    && echo checking update for 1.12-standard-1.0 >&2 \
+    && echo checking update for 1.13-standard-1.0 >&2 \
     && ./update.pl 1.13
 )
 (
     cd ubuntu/2.0 \
     && echo checking update for 1.11-standard-2.0 >&2 \
     && ./update.pl 1.11 \
-    && echo checking update for 1.11-standard-2.0 >&2 \
-    && ./update.pl 1.12 \
     && echo checking update for 1.12-standard-2.0 >&2 \
+    && ./update.pl 1.12 \
+    && echo checking update for 1.13-standard-2.0 >&2 \
     && ./update.pl 1.13
 )
 (
@@ -24,6 +24,6 @@
     && ./update.pl 1.11 \
     && echo checking update for 1.12-amazonlinux2-1.0 >&2 \
     && ./update.pl 1.12 \
-    && echo checking update for 1.12-amazonlinux2-1.0 >&2 \
+    && echo checking update for 1.13-amazonlinux2-1.0 >&2 \
     && ./update.pl 1.13
 )
