9363de17fdac90f50aae5bd6b4b735e361aac8e2
Ichinose Shogo <shogo82148@gmail.com>
2020-02-26 07:55:27 +0900

    Go 1.14 is released
    https://groups.google.com/forum/#!msg/golang-announce/AYd4cXYG8qk/FPTwU0jPCQAJ

--- update.sh
+++ update.sh
@@ -5,33 +5,43 @@
     && echo checking update for 1.12-standard-1.0 >&2 \
     && ./update.pl 1.12 \
     && echo checking update for 1.13-standard-1.0 >&2 \
-    && ./update.pl 1.13
+    && ./update.pl 1.13 \
+    && echo checking update for 1.14-standard-1.0 >&2 \
+    && ./update.pl 1.14
 ) || exit 1
 (
     cd ubuntu/2.0 \
     && echo checking update for 1.12-standard-2.0 >&2 \
     && ./update.pl 1.12 \
     && echo checking update for 1.13-standard-2.0 >&2 \
-    && ./update.pl 1.13
+    && ./update.pl 1.13 \
+    && echo checking update for 1.14-standard-2.0 >&2 \
+    && ./update.pl 1.14
 ) || exit 1
 (
     cd ubuntu/3.0 \
     && echo checking update for 1.12-standard-3.0 >&2 \
     && ./update.pl 1.12 \
     && echo checking update for 1.13-standard-3.0 >&2 \
-    && ./update.pl 1.13
+    && ./update.pl 1.14 \
+    && echo checking update for 1.14-standard-3.0 >&2 \
+    && ./update.pl 1.14
 ) || exit 1
 (
     cd al2/1.0 \
     && echo checking update for 1.12-amazonlinux2-1.0 >&2 \
     && ./update.pl 1.12 \
     && echo checking update for 1.13-amazonlinux2-1.0 >&2 \
-    && ./update.pl 1.13
+    && ./update.pl 1.13 \
+    && echo checking update for 1.14-amazonlinux2-1.0 >&2 \
+    && ./update.pl 1.14
 ) || exit 1
 (
     cd al2/2.0 \
     && echo checking update for 1.12-amazonlinux2-2.0 >&2 \
     && ./update.pl 1.12 \
     && echo checking update for 1.13-amazonlinux2-2.0 >&2 \
-    && ./update.pl 1.13
+    && ./update.pl 1.14 \
+    && echo checking update for 1.14-amazonlinux2-2.0 >&2 \
+    && ./update.pl 1.14
 ) || exit 1
