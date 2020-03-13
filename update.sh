2c87898a111764c08c0fe6272b8d6f7f858f112a
Ichinose Shogo <shogo82148@gmail.com>
2020-03-13 15:29:06 +0900

    fix golang version

--- update.sh
+++ update.sh
@@ -17,7 +17,7 @@
 (
     cd ubuntu/3.0 \
     && echo checking update for 1.13-standard-3.0 >&2 \
-    && ./update.pl 1.14 \
+    && ./update.pl 1.13 \
     && echo checking update for 1.14-standard-3.0 >&2 \
     && ./update.pl 1.14
 ) || exit 1
@@ -31,14 +31,14 @@
 (
     cd al2/2.0 \
     && echo checking update for 1.13-amazonlinux2-2.0 >&2 \
-    && ./update.pl 1.14 \
+    && ./update.pl 1.13 \
     && echo checking update for 1.14-amazonlinux2-2.0 >&2 \
     && ./update.pl 1.14
 ) || exit 1
 (
     cd al2/3.0 \
     && echo checking update for 1.13-amazonlinux2-3.0 >&2 \
-    && ./update.pl 1.14 \
+    && ./update.pl 1.13 \
     && echo checking update for 1.14-amazonlinux2-3.0 >&2 \
     && ./update.pl 1.14
 ) || exit 1
