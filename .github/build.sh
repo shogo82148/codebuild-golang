26f57fff839c9588ceb3d19ef244fd986f81e8bb
Ichinose Shogo <shogo82148@gmail.com>
2021-07-09 21:15:10 +0900

    drop publishing docker.pkg.github.com

--- .github/build.sh
+++ .github/build.sh
@@ -5,7 +5,6 @@ set -uex
 ROOT=$(cd "$(dirname "$0")" && cd .. && pwd)
 GOVERSION=$1
 TAG=${USERNAME}/codebuild-golang:${GOVERSION}-${BASE_IMAGE}-${BASE_IMAGE_VERSION}
-PACKAGE=docker.pkg.github.com/${GITHUB_REPOSITORY}/go${GOVERSION}-${BASE_IMAGE}-${BASE_IMAGE_VERSION}:latest
 
 # build
 cd "$ROOT"
@@ -16,10 +15,4 @@ if [[ ${GITHUB_REF} = 'refs/heads/main' ]]; then
     printenv PASSWORD | docker login -u "$USERNAME" --password-stdin
     docker push "$TAG"
     docker logout
-
-    # Publish to the GitHub package registry
-    printenv TOKEN | docker login docker.pkg.github.com -u "$USERNAME" --password-stdin
-    docker tag "$TAG" "$PACKAGE"
-    docker push "$PACKAGE"
-    docker logout docker.pkg.github.com
 fi
