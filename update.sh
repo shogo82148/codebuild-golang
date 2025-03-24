93bb39b4a0ecfb5b2a5c7ca603d60cc912b3ef6e
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2025-03-24 17:05:26 +0900

    bump Go 1.24

--- update.sh
+++ update.sh
@@ -2,41 +2,41 @@
 
 (
     cd ubuntu/3.0 \
+    && echo checking update for 1.24-standard-3.0 >&2 \
+    && ./update.pl 1.24 \
     && echo checking update for 1.23-standard-3.0 >&2 \
     && ./update.pl 1.23 \
-    && echo checking update for 1.22-standard-3.0 >&2 \
-    && ./update.pl 1.22 \
     && true
 ) || exit 1
 (
     cd ubuntu/4.0 \
+    && echo checking update for 1.24-standard-4.0 >&2 \
+    && ./update.pl 1.24 \
     && echo checking update for 1.23-standard-4.0 >&2 \
     && ./update.pl 1.23 \
-    && echo checking update for 1.22-standard-4.0 >&2 \
-    && ./update.pl 1.22 \
     && true
 ) || exit 1
 (
     cd ubuntu/5.0 \
+    && echo checking update for 1.24-standard-5.0 >&2 \
+    && ./update.pl 1.24 \
     && echo checking update for 1.23-standard-5.0 >&2 \
     && ./update.pl 1.23 \
-    && echo checking update for 1.22-standard-5.0 >&2 \
-    && ./update.pl 1.22 \
     && true
 ) || exit 1
 (
     cd al2/2.0 \
+    && echo checking update for 1.24-amazonlinux2-2.0 >&2 \
+    && ./update.pl 1.24 \
     && echo checking update for 1.23-amazonlinux2-2.0 >&2 \
     && ./update.pl 1.23 \
-    && echo checking update for 1.22-amazonlinux2-2.0 >&2 \
-    && ./update.pl 1.22 \
     && true
 ) || exit 1
 (
     cd al2/3.0 \
+    && echo checking update for 1.24-amazonlinux2-3.0 >&2 \
+    && ./update.pl 1.24 \
     && echo checking update for 1.23-amazonlinux2-3.0 >&2 \
     && ./update.pl 1.23 \
-    && echo checking update for 1.22-amazonlinux2-3.0 >&2 \
-    && ./update.pl 1.22 \
     && true
 ) || exit 1
